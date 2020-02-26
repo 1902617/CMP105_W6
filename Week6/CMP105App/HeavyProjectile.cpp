20a7b8b3b803689e38125da6d47cb1e6c46729c9
Jamie <56042392+jamieals99@users.noreply.github.com>
2020-02-26 10:35:54 +0000

    Update HeavyProjectile.cpp

--- Week6/CMP105App/HeavyProjectile.cpp
+++ Week6/CMP105App/HeavyProjectile.cpp
@@ -4,11 +4,11 @@
 
 HeavyProjectile::HeavyProjectile()
 {
-	hasFired = false;
-	mouseDown = false;
-	ballSelected = false;
-	mouseStartPos = sf::Vector2f(0.f, 0.f);
-	mouseEndPos = sf::Vector2f(0.f, 0.f);
+	hasFired = false;													// Change when the ball is in a movement state.
+	mouseDown = false;													// Tracks current mouse position.
+	ballSelected = false;												// Change when the cursor has selected the balls position.
+	mouseStartPos = sf::Vector2f(0.f, 0.f);								// Mouse's starting position when ball is clicked.
+	mouseEndPos = sf::Vector2f(0.f, 0.f);								// End position for the mouse drag.
 }
 
 HeavyProjectile::~HeavyProjectile()
@@ -18,26 +18,25 @@ HeavyProjectile::~HeavyProjectile()
 
 void HeavyProjectile::update(float dt, int wX, int wY)
 {
-	if (hasFired)
+	if (hasFired)														
 	{
-		sf::Vector2f trajectory = (mouseStartPos - mouseEndPos);
-		float force = (Vector::magnitude(trajectory) * 10);
-		trajectory = Vector::normalise(trajectory);
-		sf::Vector2f velocity = (trajectory * force);
-		setPosition(getPosition() + (velocity * dt));
-
-		Entity::update(dt, wX, wY);
+		sf::Vector2f trajectory = (mouseStartPos - mouseEndPos);		// Trajectory = direction the ball will be fired in.
+		float force = (Vector::magnitude(trajectory) * 10);				// Force = how fast the ball will go (x10 as base magnitude is low). 
+		trajectory = Vector::normalise(trajectory);						// Normalise the trajectory.
+		sf::Vector2f velocity = (trajectory * force);					// Calculates the balls direction and speed.
+		setPosition(getPosition() + (velocity * dt));					// Applies the movement to the ball.
+						
+		Entity::update(dt, wX, wY);										// Runs base's update that applies gravity.
 	}
 }
 
 void HeavyProjectile::handleInput(float dt, Input* i)
 {
-	if (i->isMouseLDown() && !mouseDown)
+	if (i->isMouseLDown() && !mouseDown)								// Checks for when the L mouse button is pressed		
 	{
 		mouseStartPos = sf::Vector2f(i->getMouseX(), i->getMouseY());
-		
 
-		if((mouseStartPos.x > (getPosition().x + getSize().x / 2) 
+		if((mouseStartPos.x > (getPosition().x + getSize().x / 2)		// This checks that the player hasent clicked on the actual ball.
 			|| mouseStartPos.x < (getPosition().x - getSize().x / 2)) 
 				||(mouseStartPos.y > (getPosition().y + getSize().x / 2)
 					|| mouseStartPos.y < (getPosition().y - getSize().x / 2)))
@@ -45,24 +44,24 @@ void HeavyProjectile::handleInput(float dt, Input* i)
 			//std::cout << "You have to click on the ball!\n";
 		}
 
-		else
+		else															// Else the player must have clicked the ball.
 		{
-			ballSelected = true;
-			std::cout << "Ball Selected\n";
-			mouseDown = true;
+			ballSelected = true;		
+			//std::cout << "Ball Selected\n";
+			mouseDown = true;												
 		}
 	}
 
-	if (mouseDown && ballSelected)
+	if (mouseDown && ballSelected)										// If the mouse is down and on the ball.
 	{
-		mouseEndPos = sf::Vector2f(i->getMouseX(), i->getMouseY());
+		mouseEndPos = sf::Vector2f(i->getMouseX(), i->getMouseY());		// Generate the end position to be the mouse's current position.
 		std::cout << "Drag In Progress\n";
 	}
 
-	if (!(i->isMouseLDown()) && mouseDown)
+	if (!(i->isMouseLDown()) && mouseDown)								// When the mouse is released in the drag phase.
 	{
 		std::cout << "Ball Fired\n";
 		mouseDown = false;
-		hasFired = true;
+		hasFired = true;						
 	}
 }
